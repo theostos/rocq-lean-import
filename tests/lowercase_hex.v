36922055ed601e7d83847fb4369c89a3571fbd62
theostos <theo.stoskopf@proton.me>
2026-07-24 17:26:58 +0200

    Report malformed hexadecimal bytes

--- tests/lowercase_hex.v
+++ tests/lowercase_hex.v
@@ -3,3 +3,6 @@ From LeanImport Require Import Lean.
 (* lean4export emits uppercase bytes, but the format is hexadecimal rather
    than specifically uppercase hexadecimal. *)
 Redirect "lowercase_hex.log" Lean Import "../dumps/lowercase_hex".
+
+Fail Lean Import "../dumps/invalid_hex".
+Fail Lean Import "../dumps/invalid_hex_width".
