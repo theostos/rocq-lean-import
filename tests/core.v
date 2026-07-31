8d16a4840d073e45c0582fa1387a2d505944d572
theostos <theo.stoskopf@proton.me>
2026-07-31 10:42:59 +0200

    Regenerate the core dump for modern UInt32
    Replace the legacy core fixture with a Lean 4.29 Init.Prelude export and assert the BitVec-backed UInt32 and Char shapes.

--- tests/core.v
+++ tests/core.v
@@ -1,57 +1,26 @@
 From LeanImport Require Import Lean.
 
-(* Set Lean Upfront Instantiation. *)
-Redirect "core1.log" Lean Import "../dumps/core" 1 12434.
+(* Generated from [Init.Prelude] with Lean 4.29 and lean4export 3.1.0. *)
+Redirect "core1.log" Lean Import "../dumps/core" 1 19081.
 
-(* theorem usize_size_eq :
-USize.size = 4294967296 ∨ USize.size = 18446744073709551616  is too slow *)
-Redirect "core2.log" Fail Timeout 1 Lean Import "../dumps/core" 12434 12435.
+Check UInt32_toBitVec : UInt32 -> BitVec 32.
+Check val1 : Char -> UInt32.
 
-Redirect "core3.log" Lean Import "../dumps/core" 12435 12522.
+(* The large proof generated for [isValidChar_UInt32.match_1_1] currently
+   overflows the Rocq stack. *)
+Redirect "core2.log" Fail Lean Import "../dumps/core" 19081 19082.
 
-(* Error at line 12522 (for USize.toUInt64.proof_1): #DEF 1441 10569 10615
-missing usize_size_eq *)
-Redirect "core4.log" Fail Lean Import "../dumps/core" 12522 12523.
+Redirect "core3.log" Lean Import "../dumps/core" 19082 19415.
 
-Redirect "core5.log" Lean Import "../dumps/core" 12523 12532.
+(* Depends on [_private.Init.Prelude0.isValidChar_UInt32.match_1_1]. *)
+Redirect "core4.log" Fail Lean Import "../dumps/core" 19415 19416.
 
-(* Error at line 12532 (for USize.toUInt64): #DEF 1361 10616 10624
-missing USize.toUInt64.proof_1 *)
-Redirect "core6.log" Fail Lean Import "../dumps/core" 12532 12533.
+Redirect "core5.log" Lean Import "../dumps/core" 19416 19422.
 
-Redirect "core7.log" Lean Import "../dumps/core" 12533 14481.
+(* Depends on [_private.Init.Prelude0.isValidChar_UInt32]. *)
+Redirect "core6.log" Fail Lean Import "../dumps/core" 19422 19423.
 
-(* Error:
-Error at line 14481 (for Quotient): #DEF 1613 12306 12311 3
-missing Quot *)
-Redirect "core8.log" Fail Lean Import "../dumps/core" 14481 63566.
+Redirect "core7.log" Lean Import "../dumps/core" 19423 19437.
 
-
-(* Unset Lean Upfront Instantiation. *)
-(* Redirect "core12.log" Lean Import "../dumps/core" 5464 5487. *)
-(* Set Lean Upfront Instantiation. *)
-(* Redirect "core13.log" Lean Import "../dumps/core" 5487 5584. *)
-
-(* Redirect "core2.log" Fail Lean Import "../dumps/core" 5584 63566. *)
-
-(* Print *)
-(* Redirect "core3.log" Lean Import "../dumps/core" 5086 63566. *)
-(*
-Set Printing Universes.
-Print String.
-Print Char.
-Print UInt32.
-Eval hnf in LT_lt_inst1 Nat instLTNat.
-Print Nat_le.
-Print Or.
-Print And.
-Print Fin.
-Print UInt32_isValidChar.
-Print Nat_isValidChar.
-Print UInt32_toNat.
-Print LT_lt_inst1.
-Print instLTNat.
-Print LT_inst1. *)
-(* Compute Z.of_nat (nat_of_Nat UInt32_size. *)
-(* n < 0xd800 ∨ (0xdfff < n ∧ n < 0x110000) *)
-(* Eval hnf in UInt32_isValidChar _. *)
+(* Depends on [Char.ofNatAux._private_1]. *)
+Redirect "core8.log" Fail Lean Import "../dumps/core" 19437 19438.
